cd33e098ccdd06bdd2ef88875309e7f4d06fe1df
Krasimir Dermendzhiev <k.d.sf.ltd@gmail.com>
2023-05-15 10:16:59 +0300

    Change the name

--- links/src/main/java/payroll/LoadDatabase.java
+++ links/src/main/java/payroll/LoadDatabase.java
@@ -16,7 +16,7 @@ class LoadDatabase {
 
 		return args -> {
 			employeeRepository.save(new Employee("First", "Second", "Third"));
-			employeeRepository.save(new Employee("Second", "Baggins", "thief"));
+			employeeRepository.save(new Employee("First", "Baggins", "thief"));
 
 			employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
 
